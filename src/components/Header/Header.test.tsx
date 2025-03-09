d0c2b7f858eb7fd2d326694a778702266a126929
Scott Rogener <srogener@gmail.com>
2025-03-08 19:45:01 -0500

    fix tests

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 
 import { Header } from './Header';
+import { describe, expect, it } from 'vitest';
 
 describe('Header', () => {
   it('renders with default title', () => {
