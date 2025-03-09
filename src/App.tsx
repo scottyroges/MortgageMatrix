942da71159d9e2fec0505cf77ecbc8e70e70d904
Scott Rogener <srogener@gmail.com>
2025-03-09 16:06:10 -0400

    fixing build issue

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { PrivacyPolicy } from './pages/PrivacyPolicy'
 import { TermsOfService } from './pages/TermsOfService'
 import { ScrollToTop } from './components/ScrollToTop'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './App.css'
 
 const App = () => {
